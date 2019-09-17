2a62638c88a9163e32cfb582c4129a713df4550c
GedMarc <ged_marc@hotmail.com>
2019-09-17 06:22:27 +0200

    JBoss Logging and XNIO to Modular

--- undertow/core/src/main/java/module-info.java
+++ undertow/core/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module undertow.core {
 	requires static java.security.jgss;
 
 	requires static alpn.api;
-	requires static io.undertow.parser.generator;
+//	requires static io.undertow.parser.generator;
 	requires static jdk.unsupported;
 	requires static java.naming;
 	requires static java.sql;
